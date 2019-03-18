154c01f91db0711e62d9a8b2afab8d91c2a3f2f9
Evgeniy Bondarenko <Bondarenko.Hub@gmail.com>
2019-03-19 02:26:53 +0300

    Add CI. Update build stage. Update Pipfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,7 @@ export DB_NAME=${DB_NAME:-"intsys"}
 export DB_USER=${DB_USER:-"intsys"}
 export DB_PASSWORD=${DB_PASSWORD:-"intsys"}
 export SENTRY_DSN=${SENTRY_DSN:-"https://ec33c4a0117b4faea012fd7422ebcee3:7b69d46d2e834a5d8c4a646d9837bba1@sentry-bifok7.2035.university/19"}
-export SENTRY_ENVIRONMENT=${SENTRY_ENVIRONMENT:-intsys-health-check-stage}"
+export SENTRY_ENVIRONMENT=${SENTRY_ENVIRONMENT:-"intsys-health-check-stage"}
 
 echo starting
 
