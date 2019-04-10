c575e4ac53ec5628f23bf1593f0af440b3516dd7
Evgeniy Bondarenko <Bondarenko.Hub@gmail.com>
2019-04-10 22:33:46 +0300

    update for CI

--- docker-builder.sh
+++ docker-builder.sh
@@ -1,11 +1,18 @@
-#!/bin/bash
+#!/bin/bash -e
 # by Evgeniy Bondarenko <Bondarenko.Hub@gmail.com>
-# last changes 06.01.2018 Create
+# last changes 01.03.2019 Updated
 
-dockerhub='docker.u2035s.ru/'
-name="functional-checks"
-tag="latest"
-#version="_$(date +%Y-%m-%d_%H-%M-%S)"
 
-docker build -t ${dockerhub}${name}:${tag} -t ${dockerhub}${name}:${tag}${version}  . && \
-docker push ${dockerhub}${name}:${tag}${version} && docker push ${dockerhub}${name}:${tag}
+dockerhub=${dockerhub:-"docker.u2035s.ru"}
+name=${name:-"unti/functional-checks"}
+tag=${tag:-"stage"}
+version="_$(date +%Y-%m-%d_%H-%M-%S)"
+NO_CACHE=${NO_CACHE:-"random"}
+
+docker build --build-arg NO_CACHE=${NO_CACHE}  -t ${dockerhub}/${name}:latest -t ${dockerhub}/${name}:${tag} -t ${dockerhub}/${name}:${tag}${version}  .
+
+if [ ${tag} == 'prod' ]; then
+docker push ${dockerhub}/${name}:${tag}${version} && docker push ${dockerhub}/${name}:${tag}
+else
+docker push ${dockerhub}/${name}:${tag} && docker push ${dockerhub}/${name}:latest
+fi
