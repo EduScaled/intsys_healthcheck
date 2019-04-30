a523a00603872fded398290a040dedc47f02987f
Vladimir Mikryukov <mikryukov-vv@sberbank.ru>
2019-04-30 18:27:14 +0300

    Implemented health checks for Carrier

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+import json
+
+import aiopg
 import asyncio
 from typing import Callable
 
+import datetime
 import sentry_sdk
 from aiohttp import web
 from sentry_sdk import capture_exception
 from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 
+from checks.carrier import CarrierCheck
 from checks.culture import CultureCheck
 from checks.postgres import PostgresResponseCheck
 from settings import settings
@@ -41,11 +46,69 @@ async def culture_healthcheck(_):
 
     return web.json_response(result, status=status)
 
+async def carrier_healthcheck(_):
+    result = await CarrierCheck(
+        settings.DB_HOST, settings.DB_PORT, settings.DB_NAME, settings.DB_USER,  settings.DB_PASSWORD
+    ).check()
+    status = 200 if str(result.get("status", None)) == "200" else 500
+
+    return web.json_response(result, status=status)
+
+async def carrier_callback(request):
+    json_request = await request.json()
+    if json_request['topic'] == 'healthcheck':
+        pool = await aiopg.create_pool("host={} port={} dbname={} user={} password={}".format(
+            settings.DB_HOST, settings.DB_PORT, settings.DB_NAME, settings.DB_USER, settings.DB_PASSWORD
+        ))
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute("SELECT message FROM carrier_message")
+                query_result = await cur.fetchall()
+                if query_result[0][0] == json_request['value']:
+                    kafka_read_result = {
+                        "carrier-kafka-read": True,
+                        "status": 200,
+                        "created_at": str(datetime.datetime.utcnow())
+                    }
+                    await cur.execute(
+                        "INSERT INTO carrier_status (result) VALUES ('{}')".format(
+                            json.dumps(kafka_read_result)
+                        ))
+                    conn.commit()
+                else:
+                    pass
+
+    return web.Response(status=200)
+
+async def set_settings(request):
+    update_query = """
+    INSERT INTO settings (name, value)
+    VALUES ('is_required', '{value}')
+    ON CONFLICT (name)
+    DO UPDATE SET
+    value='{value}';
+    """
+    post_data = await request.json()
+    is_required = post_data.get('is_required', None)
+    if is_required is not None:
+        pool = await aiopg.create_pool("host={} port={} dbname={} user={} password={}".format(
+            settings.DB_HOST, settings.DB_PORT, settings.DB_NAME, settings.DB_USER, settings.DB_PASSWORD
+            ))
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(update_query.format(value=is_required))
+                conn.commit()
+        return web.Response(status=200)
+
+    return web.Response(status=400, text='Неизвестные параметры запроса.')
 
 def init_func():
     app = web.Application()
     app.add_routes([
         web.get('/healthcheck/culture', culture_healthcheck),
         web.get('/healthcheck/db', db_healthcheck),
+        web.get('/healthcheck/carrier', carrier_healthcheck),
+        web.post('/healthcheck/set_settings', set_settings),
+        web.post('/carrier/callback', carrier_callback),
     ])
     return app
---
+++ checks/carrier.py
@@ -0,0 +1,51 @@
+import json
+import aiopg
+import asyncio
+from datetime import datetime, timedelta
+
+
+class CarrierCheck:
+
+    def __init__(self, host, port, dbname, user, password) -> None:
+        super().__init__()
+        self.dsn = "host={} port={} dbname={} user={} password={}".format(
+            host, port, dbname, user, password
+        )
+
+    async def fetch_result(self, cursor):
+        await cursor.execute("SELECT result FROM carrier_status;")
+        return await cursor.fetchall()
+
+    async def carrier_healthcheck(self):
+        checks = []
+        pool = await aiopg.create_pool(self.dsn)
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                query_result = await self.fetch_result(cur)
+                if len(query_result) < 2:
+                    asyncio.sleep(1)
+                    query_result = await self.fetch_result(cur)
+
+                if len(query_result) == 2:
+                    for row in query_result:
+                        data = json.loads(row[0])
+                        if data.get("created_at", None):
+                            parsed = datetime.fromisoformat(data.get("created_at"))
+                            if (datetime.utcnow() - timedelta(minutes=2)) <= parsed:
+                                checks.append(True if data.get("status", None) == 200 else False)
+                            else:
+                                checks.append(False)
+                        else:
+                            checks.append(False)
+                else:
+                    checks.append(False)
+
+        return {
+        'status': 200 if all(checks) else 500,
+        "carrier": all(checks),
+        "created_at": str(datetime.utcnow())
+        }
+
+
+    async def check(self):
+        return await self.carrier_healthcheck()
