92bdb58b02ae1b5a2847c462154d49cdc6320291
Nick Lubyanov <lubyanov@gmail.com>
2019-05-30 17:23:21 +0300

    db check json response

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ async def run_check(f: Callable, **kwargs):
     try:
         return await f(**kwargs)
     except Exception as e:
-        print(e)
-        # capture_exception(e)
+        capture_exception(e)
         return False
 
 async def db_healthcheck(_):
@@ -33,8 +32,9 @@ async def db_healthcheck(_):
         settings.DB_HOST, settings.DB_PORT, settings.DB_NAME, settings.DB_USER,  settings.DB_PASSWORD
     ).check()
     status = 200 if result else 500
-
-    return web.Response(status=status)
+    result = { "status": status, "db": str(status == 200).lower() }
+    
+    return web.json_response(result, status=status)
 
 async def culture_healthcheck(_):
     result = await CultureCheck(
